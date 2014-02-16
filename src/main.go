9184d1e31f497e73c8e905c7d1b6944bb4a35b0b
Kai Richard Koenig <kai.richard.koenig@googlemail.com>
2014-02-16 16:47:02 +0100

    using go fmt

--- src/main.go
+++ src/main.go
@@ -1,71 +1,70 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "time"
-  "path"
-  "flag"
-  "runtime"
-  "io/ioutil"
-  "net/http"
-  "encoding/json"
-  "github.com/fzzy/radix/redis"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/fzzy/radix/redis"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+	"runtime"
+	"time"
 )
 
 var redis_location = flag.String("redis", "127.0.0.1:6379", "Location of redis instance")
 var server_bind = flag.String("bind", "127.0.0.1:5000", "Location server should listen at")
 var handler_mount = flag.String("mount", "/", "Relative path where handler should be at")
 
-
 var client *redis.Client
 
 type RequestInfo struct {
-  Query string
-  UserAgent string
-  Time int64
+	Query     string
+	UserAgent string
+	Time      int64
 }
 
-func initStorage(db int){
-  var err error
-  client, err = redis.DialTimeout("tcp", *redis_location, time.Duration(10)*time.Second)
-  errHndlr(err)
-  client.Cmd("SELECT", db)
+func initStorage(db int) {
+	var err error
+	client, err = redis.DialTimeout("tcp", *redis_location, time.Duration(10)*time.Second)
+	errHndlr(err)
+	client.Cmd("SELECT", db)
 }
 
 func errHndlr(err error) {
-  if err != nil {
-    fmt.Println("error:", err)
-    os.Exit(1)
-  }
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
 }
 
 func httpStore(res http.ResponseWriter, req *http.Request) {
-  ri := RequestInfo{
-    Query: req.URL.RawQuery,
-    UserAgent: req.UserAgent(),
-    Time: time.Now().Unix(),
-  }
+	ri := RequestInfo{
+		Query:     req.URL.RawQuery,
+		UserAgent: req.UserAgent(),
+		Time:      time.Now().Unix(),
+	}
 
-  if ri.Query != ""{
-    b, err := json.Marshal(ri)
-    errHndlr(err)
-    client.Cmd("RPUSH", "incoming", string(b))
-  }
+	if ri.Query != "" {
+		b, err := json.Marshal(ri)
+		errHndlr(err)
+		client.Cmd("RPUSH", "incoming", string(b))
+	}
 
-  _, filename, _, _ := runtime.Caller(0)
-  beacon, err := ioutil.ReadFile(path.Join(path.Dir(filename), "../assets/1x1.gif"))
-  errHndlr(err)
+	_, filename, _, _ := runtime.Caller(0)
+	beacon, err := ioutil.ReadFile(path.Join(path.Dir(filename), "../assets/1x1.gif"))
+	errHndlr(err)
 
-  res.Header().Set("Content-Type", "image/gif")
-  res.Write(beacon)
+	res.Header().Set("Content-Type", "image/gif")
+	res.Write(beacon)
 }
 
-func main(){
-  flag.Parse()
-  initStorage(1)
-  http.HandleFunc(*handler_mount, httpStore)
+func main() {
+	flag.Parse()
+	initStorage(1)
+	http.HandleFunc(*handler_mount, httpStore)
 	err := http.ListenAndServe(*server_bind, nil)
-  errHndlr(err)
-  defer client.Close()
+	errHndlr(err)
+	defer client.Close()
 }
--- src/main_test.go
+++ src/main_test.go
@@ -1,67 +1,66 @@
 package main
 
 import (
-    "testing"
-    "net/http"
-    "net/http/httptest"
-    "encoding/json"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 func init() {
-  initStorage(99)
+	initStorage(99)
 }
 
-func tearDown(){
-  client.Cmd("FLUSHDB")
+func tearDown() {
+	client.Cmd("FLUSHDB")
 }
 
 func TestHandlerRespondsWithAGif(t *testing.T) {
-    defer tearDown()
-    q := "a=test&b=test"
-    request, _ := http.NewRequest("GET", "/?" + q, nil)
-    response := httptest.NewRecorder()
+	defer tearDown()
+	q := "a=test&b=test"
+	request, _ := http.NewRequest("GET", "/?"+q, nil)
+	response := httptest.NewRecorder()
 
-    httpStore(response, request)
-    ctype := response.HeaderMap.Get("Content-Type")
-    if ctype != "image/gif"{
-        t.Fatalf("Content-Type expected %v:\n Got: %v", "image/gif", ctype)
-    }
+	httpStore(response, request)
+	ctype := response.HeaderMap.Get("Content-Type")
+	if ctype != "image/gif" {
+		t.Fatalf("Content-Type expected %v:\n Got: %v", "image/gif", ctype)
+	}
 }
 
 func TestHandlerDoesNotRecordAnythingWhenThereIsNoQuery(t *testing.T) {
-    defer tearDown()
-    request, _ := http.NewRequest("GET", "/", nil)
-    response := httptest.NewRecorder()
+	defer tearDown()
+	request, _ := http.NewRequest("GET", "/", nil)
+	response := httptest.NewRecorder()
 
-    httpStore(response, request)
-    length, _ := client.Cmd("LLEN", "incoming").Int()
-    if length != 0 {
-      t.Fatalf("List should not contain entry")
-    }
+	httpStore(response, request)
+	length, _ := client.Cmd("LLEN", "incoming").Int()
+	if length != 0 {
+		t.Fatalf("List should not contain entry")
+	}
 }
 
-
 func TestAllQueryParamsAreStored(t *testing.T) {
-    defer tearDown()
-    q := "a=test&b=test"
-    request, _ := http.NewRequest("GET", "/?" + q, nil)
-    response := httptest.NewRecorder()
+	defer tearDown()
+	q := "a=test&b=test"
+	request, _ := http.NewRequest("GET", "/?"+q, nil)
+	response := httptest.NewRecorder()
 
-    httpStore(response, request)
+	httpStore(response, request)
 
-    if response.Code != http.StatusOK {
-        t.Fatalf("Response body did not contain expected %v:\n\tbody: %v", "200", response.Code)
-    }
+	if response.Code != http.StatusOK {
+		t.Fatalf("Response body did not contain expected %v:\n\tbody: %v", "200", response.Code)
+	}
 
-    length, _ := client.Cmd("LLEN", "incoming").Int()
-    if length != 1 {
-      t.Fatalf("List should not have zero entries")
-    }
+	length, _ := client.Cmd("LLEN", "incoming").Int()
+	if length != 1 {
+		t.Fatalf("List should not have zero entries")
+	}
 
-    res, _ := client.Cmd("LPOP", "incoming").Bytes()
-    var ri RequestInfo
-    json.Unmarshal(res, &ri)
-    if ri.Query != q {
-      t.Fatalf("Query does not match %+v", ri)
-    }
+	res, _ := client.Cmd("LPOP", "incoming").Bytes()
+	var ri RequestInfo
+	json.Unmarshal(res, &ri)
+	if ri.Query != q {
+		t.Fatalf("Query does not match %+v", ri)
+	}
 }
